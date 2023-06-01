5758e222463184ef2cdb9e76517f8e26eb71cb13
Viiitor <vitoruemura@hotmail.com>
2023-05-31 21:55:50 -0300

    Alteração (categorias)

--- lib/view/manga_form.dart
+++ lib/view/manga_form.dart
@@ -6,20 +6,22 @@ import 'package:mdesk/view/widget/botao.dart';
 import 'package:mdesk/view/widget/campo_descricao.dart';
 import 'package:mdesk/view/widget/campo_nome.dart';
 import 'package:mdesk/view/widget/campo_url.dart';
+import 'package:mdesk/view/dto/categoria.dart';
 
-  class MangaForm extends StatefulWidget{
-  const MangaForm({Key? key}) : super(key: key);
+class MangaForm extends StatefulWidget {
+  MangaForm({Key? key}) : super(key: key);
 
   @override
   State<MangaForm> createState() => _MangaFormState();
 }
 
-  class _MangaFormState extends State<MangaForm> {
-    final formKey = GlobalKey<FormState>();
-    dynamic id;
+class _MangaFormState extends State<MangaForm> {
+  final formKey = GlobalKey<FormState>();
+  dynamic id;
+  
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     receberMangaParaAlteracao(context);
     return Scaffold(
       appBar: AppBar(title: const Text('Cadastro')),
@@ -30,7 +32,6 @@ import 'package:mdesk/view/widget/campo_url.dart';
             campoNome,
             campoDescricao,
             campoURL,
-            criarBotao(context),
           ],
         ),
       ),
@@ -40,46 +41,45 @@ import 'package:mdesk/view/widget/campo_url.dart';
   final campoNome = CampoNome(controle: TextEditingController());
   final campoDescricao = CampoDescricao(controle: TextEditingController());
   final campoURL = CampoURL(controle: TextEditingController());
+  Categoria? categoriaSelecionada;
 
-  Widget criarBotao(BuildContext context){
+  Widget criarBotao(BuildContext context) {
     return Botao(
       context: context,
-      salvar: (){
+      salvar: () {
         var formState = formKey.currentState;
-        if(formState != null && formState.validate()){
+        if (formState != null && formState.validate()) {
           var manga = preencherDTO();
           MangaInterfaceDAO dao = MangaDAOSQLite();
           dao.salvar(manga);
           Navigator.pop(context);
         }
-      }
+      },
     );
   }
 
-  void receberMangaParaAlteracao(BuildContext context){
+  void receberMangaParaAlteracao(BuildContext context) {
     var parametro = ModalRoute.of(context);
-    if(parametro != null && parametro.settings.arguments != null){
+    if (parametro != null && parametro.settings.arguments != null) {
       Manga manga = parametro.settings.arguments as Manga;
       id = manga.id;
       preencherCampos(manga);
     }
   }
 
-  Manga preencherDTO(){
+  Manga preencherDTO() {
     return Manga(
       id: id,
       nome: campoNome.controle.text,
       descricao: campoDescricao.controle.text,
-      urlAvatar: campoURL.controle.text
+      urlAvatar: campoURL.controle.text,
     );
   }
 
-  void preencherCampos(Manga manga){
+  void preencherCampos(Manga manga) {
     campoNome.controle.text = manga.nome;
     campoDescricao.controle.text = manga.descricao;
     campoURL.controle.text = manga.urlAvatar;
-
-    }
   }
 
-  
+}
